74f598f266b6e99067437845910e75d137542168
Abhinayaa <abhinayaa1787@gmail.com>
2019-06-20 23:32:44 -0400

    Jaws Db connection created

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -1,13 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL){
+connection= mysql.createConnection(process.env.JAWSDB_URL);}else{
+  connection=mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Aadhav0422!",
   database: "burgers_db"
 });
-
+};
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
